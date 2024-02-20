0cc9aead1d78ea2eb7ffd2e4725e6189917caae8
ObamaBolsa <daniel.alegria.lino@gmail.com>
2024-02-20 15:22:46 -0600

    Created first structure template

---
+++ ProjectComunadesExtranjeras/ProjectComunadesExtranjeras/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ProjectComunadesExtranjeras project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProjectComunadesExtranjeras.settings')
+
+application = get_asgi_application()
