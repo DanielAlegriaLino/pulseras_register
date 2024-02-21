77bed619c5456bc68e5a88677b24e043eae299e3
AbisaiBH <abizabh@gmail.com>
2024-02-21 16:53:24 -0600

    fix static files

--- ProjectComunadesExtranjeras/AppResgistro/views.py
+++ ProjectComunadesExtranjeras/AppResgistro/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render, HttpResponse
-from django.urls import get_resolver
-
 
 def home(request):
     return render(request, 'home.html')
--- ProjectComunadesExtranjeras/ProjectComunadesExtranjeras/urls.py
+++ ProjectComunadesExtranjeras/ProjectComunadesExtranjeras/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.conf import settings
 from django.urls import path
 from AppResgistro import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
     path('registros/', views.registers, name="registers"),
-]
-
-static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
